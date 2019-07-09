fbd34d383954924248a8b18686a72503fd682ef3
GshIT <alabbe@uc.edu.ve>
2019-07-09 22:15:49 +0200

    Added Product url Scrapper

--- product_links.py
+++ product_links.py
@@ -57,9 +57,9 @@ def get_name_url(html, actual):
 		temp.append(category)
 
 		if category['name'] == actual:
-			scategories = read_file('scategories.py')
+			scategories = read_file('scategories.json')
 			scategories.append(category)
-			write_file('scategories.py',scategories)
+			write_file('scategories.json',scategories)
 			temp = []
 			break
 	return temp
@@ -103,3 +103,53 @@ def change_country():
 	vc.connect('vpn')
 	print('Changin Country..')
 	time.sleep(4)
+
+def get_products(html):
+	soup = BeautifulSoup(html,'html.parser')                                                                                                                        
+	products = soup.select('ul.products-grid li a.product-image')
+	return products
+
+def copy_scategories():
+	scategories = read_file('scategories.json')
+	write_file('leftscategories.json', scategories)
+
+def get_purl(products):
+	return [product.get('href') for product in products]
+
+def paginate(html):
+	soup = BeautifulSoup(html,'html.parser')
+	nextb = soup.select_one('li.next-btn')
+	return True if nextb else False
+
+@retry('products_url.json')
+def get_product_links():
+	categories = [] if not read_file('leftscategories.json') else read_file('leftscategories.json')
+	cookies = {'CATEGORY_INFO': '{"is_ajax":"1","limit":"36"}'}
+	fstring = "?limit=36&p={}" 
+	products = [] if not read_file("products_url.json") else read_file("products_url.json")                                                                            
+
+	while categories:
+		nextp = True
+		i = read_file('state.json').get('state')
+		while nextp:
+			temp = []
+			nextp = False
+			url = categories[-1]['url']
+			print(f"Scrapping: {url}{fstring.format(i)}")
+			res = rq.get(f"{url}{fstring.format(i)}", cookies=cookies)
+			write_file('state.json', {"state": i})
+			if not res.ok: raise Exception("Categories Url Failed")
+
+			products_container = get_products(res.text)
+			purls = get_purl(products_container)
+			temp = [{'product_url': purl, 'category': categories[-1]['path'], "category_url": f"{url}{fstring.format(i)}"} for purl in purls]
+			products.extend(temp)
+			write_file('products_url.json', products)
+			if paginate(res.text): nextp = True
+			i = i + 1
+
+		categories.pop()
+		write_file('leftscategories.json',categories)
+		write_file('state.json', {"state": 1})
+
+	return products
